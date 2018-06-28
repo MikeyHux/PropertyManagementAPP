15039f74ec05072f9c90376c53c9d19995d0bf2b
MikeyHux <39528346+MikeyHux@users.noreply.github.com>
2018-06-28 17:39:45 -0400

    Added:
    Owner Profiles Activity and xml layout

---
+++ app/src/main/java/com/pmapp/mikeys/propertymanagementapp/OwnerProfilesActivity.java
@@ -0,0 +1,24 @@
+package com.pmapp.mikeys.propertymanagementapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class OwnerProfilesActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ownerprofiles);
+
+        Toast.makeText(this, "Owner Profiles Activity!",
+                Toast.LENGTH_LONG).show();
+
+
+    }
+
+
+}
--- app/src/main/java/com/pmapp/mikeys/propertymanagementapp/SettingsActivity.java
+++ app/src/main/java/com/pmapp/mikeys/propertymanagementapp/SettingsActivity.java
@@ -1,11 +1,13 @@
 package com.pmapp.mikeys.propertymanagementapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -14,20 +16,21 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Toast.makeText(this, "Settings Activity!",
                 Toast.LENGTH_LONG).show();
+
+        //Declare button
+        Button btnOwnerProfiles = (Button)findViewById(R.id.btnOwnerProfiles);
+
+        //On click listener once btnSettings is clicked we will be directed to the Settings Activity
+        btnOwnerProfiles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, OwnerProfilesActivity.class));
+            }
+        });
 
 
     }
